9225bedee1697a6d31b8a076d76230a4b840d73a
Carlos Legnaro <carlos_legnaro@outlook.com>
2020-09-25 17:31:33 -0300

    ajuste.

--- index.php
+++ index.php
@@ -12,4 +12,18 @@ $tv-> name = 'TV';
 $tv->description = 'Smart TV';
 $tv->price = 4.500;
 
-echo $tv->name;
+echo $tv->name; echo ', ';
+echo $tv->description; echo ', ';
+echo $tv->price; echo ', ';
+echo '<hr>'; 
+
+$geladeira = new Product;
+$geladeira->name = 'Geladeira';
+$geladeira->description = 'Degelo automático';
+$geladeira->price = 2.900;
+
+echo $geladeira->name; echo ', ';
+echo $geladeira->description; echo ', ';
+echo $geladeira->price; echo ', ';
+echo '<hr>';
+
