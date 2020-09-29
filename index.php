0c4a49073eb9eaf78d7d1f29b3fe2ef2a89cc413
Carlos Legnaro <carlos_legnaro@outlook.com>
2020-09-29 09:11:41 -0300

    Propriedades Estáticas PHP OO.

--- index.php
+++ index.php
@@ -5,5 +5,38 @@ class Escola
     const PID = 'FAFADE';
 }
 
-$escola01 = new Escola;
-echo $escola01::PID;
+class Aluno
+{
+    public $name;
+    private $saldo = 0.0;
+
+    public function addSaldo(float $saldo)
+    {
+        $this->saldo  = $saldo;
+    }
+
+    public function getSaldo(): float
+    {
+        return $this->saldo;
+    }
+
+    public function novaCompra(float $valor)
+    {
+        $this->saldo = $this->saldo - $valor;
+        if ($this->saldo >= $valor) {
+            $this->saldo -= $valor;
+        } else {
+            return 'Saldo insuficiênte';
+        }
+    }
+}
+
+$aluno01 = new Aluno;
+$aluno01->name = 'Carlos';
+echo $aluno01->getSaldo();
+echo '<br>';
+$aluno01->addSaldo(50.0);
+echo $aluno01->getSaldo();
+$aluno01->novaCompra(20);
+echo '<br>';
+echo $aluno01->getSaldo();
